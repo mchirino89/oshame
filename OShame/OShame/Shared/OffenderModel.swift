974dfefe9120b0cdd14de15e24c9fc125a224f70
Mauricio Chirino <mauricio@3codegeeks.com>
2018-08-25 20:09:47 -0300

    Add watch target

---
+++ OShame/OShame/Shared/OffenderModel.swift
@@ -0,0 +1,14 @@
+//
+//  CounterModel.swift
+//  OShame
+//
+//  Created by Mauricio Chirino on 25/8/18.
+//  Copyright © 2018 Mauricio Chirino. All rights reserved.
+//
+
+import Foundation
+
+struct OffenderModel {
+    var name: String
+    var offense: Int
+}
